9c3eaad7d73d6d4d920a47131ee9a5d264d258d3
weii1501 <vodacluc15@gmail.com>
2024-04-03 11:49:46 +0700

    blog task

---
+++ models/blogModel.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose"); // Erase if already required
+
+// Declare the Schema of the Mongo model
+var blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    numViews: {
+      type: Number,
+      default: 0,
+    },
+    isLiked: {
+      type: Boolean,
+      default: false,
+    },
+    isDisliked: {
+      type: Boolean,
+      default: false,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    image: {
+      type: String,
+      default:
+        "https://www.toponseek.com/blogs/wp-content/uploads/2022/06/viet-blog-3.jpg",
+    },
+    author: {
+      type: String,
+      default: "Admin",
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+    timestamps: true,
+  }
+);
+
+//Export the model
+module.exports = mongoose.model("User", blogSchema);
